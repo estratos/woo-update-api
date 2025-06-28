9328418e17c907fc91ae856f62e12219318687a3
Julio Rodriguez <julio@estratos.net>
2025-06-28 03:32:36 -0600

    return product data

--- includes/class-api-handler.php
+++ includes/class-api-handler.php
@@ -79,7 +79,7 @@ class API_Handler {
         $this->deactivate_fallback_mode();
 
         set_transient($transient_key, $data, $this->cache_time);
-        return $data;
+        return $productData;
     }
 
     private function activate_fallback_mode() {
