9f000ffedc6894ff6f54d8c0a4a17f903420d3f5
Julio Rodriguez <julio@estratos.net>
2025-07-10 03:49:04 -0600

    sugested changes

--- woo-update-api.php
+++ woo-update-api.php
@@ -60,29 +60,16 @@ class Woo_Update_API
 
         // Initialize plugin
         add_action('plugins_loaded', [$this, 'init'], 20);
-        add_action('wp_ajax_wc_update_api_reconnect', [$this, 'ajax_reconnect']);
+        
+        // Register AJAX handlers
+        add_action('wp_ajax_woo_update_api_get_status', [API_Handler::instance(), 'get_status']);
+        add_action('wp_ajax_woo_update_api_reconnect', [API_Handler::instance(), 'handle_reconnect']);
+        add_action('wp_ajax_woo_update_api_refresh_product', [Price_Updater::instance(), 'ajax_refresh_product']);
         // In your main plugin file:
-        add_action('wp_ajax_woo_update_api_get_status', function () {
-            check_ajax_referer('woo_update_api_nonce', 'security');
-
-            if (!current_user_can('manage_options')) {
-                wp_send_json_error(['message' => __('Permission denied', 'woo-update-api')], 403);
-            }
-
-            $error_manager = new API_Error_Manager();
-            $status = $error_manager->get_status();
-
-            ob_start();
-            include plugin_dir_path(__FILE__) . 'admin/partials/status-indicator.php';
-            $status_html = ob_get_clean();
-
-            wp_send_json_success([
-                'status_html' => $status_html,
-                'status' => $status
-            ]);
-        });
+        
     }
 
+    
 
 
     public function check_woocommerce_active()
